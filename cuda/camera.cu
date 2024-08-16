4fd88e203a073c0495e07b950e9b48339f929a8b
shangar21 <sharanshangar@gmail.com>
2024-08-16 12:57:56 -0400

    added background gradient, some work in debug

--- cuda/camera.cu
+++ cuda/camera.cu
@@ -1,8 +1,8 @@
+#include "ray.cu"
+#include "sphere.cu"
 #include <cuda_runtime.h>
 #include <curand_kernel.h>
 #include <iostream>
-#include "sphere.cu"
-#include "ray.cu"
 
 #define N_SAMPLES 3
 
@@ -31,7 +31,7 @@ __global__ void render_kernel(double *R, double *G, double *B,
   for (int _ = 0; _ < N_SAMPLES; _++) {
     double offset_x = curand_uniform_double(&localState) - 0.5;
     double offset_y = curand_uniform_double(&localState) - 0.5;
-		double offset_z = curand_uniform_double(&localState) - 0.5;
+    double offset_z = curand_uniform_double(&localState) - 0.5;
     ray_d_x = pixel00.x + ((i + offset_x) * pixel_delta_u.x) +
               ((j + offset_x) * pixel_delta_v.x);
     ray_d_y = pixel00.y + ((i + offset_y) * pixel_delta_u.y) +
@@ -42,32 +42,40 @@ __global__ void render_kernel(double *R, double *G, double *B,
     double3 ray_o = make_double3(0.0, 0.0, 0.0);
     double3 ray_d = make_double3(ray_d_x, ray_d_y, ray_d_z);
 
-		ray r = ray(ray_o, ray_d);
+    ray r = ray(ray_o, ray_d);
 
-		double t = 0.0;
-		double3 normal = make_double3(1.0, 0.0, 0.5);
+    double t = 0.0;
+    double3 normal = make_double3(1.0, 0.0, 0.5);
 
     if (s.hit(r, t, normal)) {
-      R[idx] = normal.x;
-      G[idx] = normal.y;
-      B[idx] = normal.z;
-    }
+      R[idx] = 0.5 * (normal.x + 1);
+      G[idx] = 0.5 * (normal.y + 1);
+      B[idx] = 0.5 * (normal.z + 1);
+    } else {
+			double3 unit_direction = unit_vector(r.dir);
+			double a = 0.5 * (unit_direction.y + 1.0);
+			R[idx] = (1.0 - a) + (a * 1.0);
+			G[idx] = (1.0 - a) + (a * 0.1);
+			B[idx] = (1.0 - a) + (a * 0.1);
+		}
   }
 }
 
 // Function to initialize CUDA-related data and call the kernel
-extern "C" void
-render_image(double *h_R, double *h_G, double *h_B, int image_width,
-             int image_height, double* pixel00_ptr, double* pixel_delta_u_ptr, double* pixel_delta_v_ptr, double radius, double* center_ptr) {
+extern "C" void render_image(double *h_R, double *h_G, double *h_B,
+                             int image_width, int image_height,
+                             double *pixel00_ptr, double *pixel_delta_u_ptr,
+                             double *pixel_delta_v_ptr, double radius,
+                             double *center_ptr) {
 
   const int num_pixels = image_width * image_height;
 
-	double3 pixel00 = doublePtrToDouble3(pixel00_ptr);
-	double3 pixel_delta_u = doublePtrToDouble3(pixel_delta_u_ptr);
-	double3 pixel_delta_v = doublePtrToDouble3(pixel_delta_v_ptr);
-	double3 center = doublePtrToDouble3(center_ptr);
+  double3 pixel00 = doublePtrToDouble3(pixel00_ptr);
+  double3 pixel_delta_u = doublePtrToDouble3(pixel_delta_u_ptr);
+  double3 pixel_delta_v = doublePtrToDouble3(pixel_delta_v_ptr);
+  double3 center = doublePtrToDouble3(center_ptr);
 
-	sphere s = sphere(center, radius);
+  sphere s = sphere(center, radius);
 
   double *d_R, *d_G, *d_B;
   curandState *d_state;
@@ -84,9 +92,9 @@ render_image(double *h_R, double *h_G, double *h_B, int image_width,
 
   // Launch the kernel to render the image
   render_kernel<<<dim3((image_width + 15) / 16, (image_height + 15) / 16),
-                  dim3(16, 16)>>>(
-      d_R, d_G, d_B, d_state, image_width, image_height, pixel00, pixel_delta_u,
-      pixel_delta_v, s);
+                  dim3(16, 16)>>>(d_R, d_G, d_B, d_state, image_width,
+                                  image_height, pixel00, pixel_delta_u,
+                                  pixel_delta_v, s);
 
   // Copy the result back to the host
   cudaMemcpy(h_R, d_R, num_pixels * sizeof(double), cudaMemcpyDeviceToHost);
--- cuda/sphere.cu
+++ cuda/sphere.cu
@@ -7,25 +7,24 @@ struct sphere {
   double3 center;
   double radius;
 
-  __host__ __device__ sphere(double3 c, double r)
-      : center(c), radius(r) {}
+  __host__ __device__ sphere(double3 c, double r) : center(c), radius(r) {}
 
-  __host__ __device__ bool hit(const ray r, double& t, double3& normal) {
+  __host__ __device__ bool hit(const ray r, double &t, double3 &normal) {
     double3 oc = center - r.orig;
     double a = length_squared(r.dir);
     double b = -2.0 * dot(r.dir, oc);
     double c = length_squared(oc) - (radius * radius);
     double discriminant = b * b - 4 * a * c;
 
-    if (discriminant < 0){
+    if (discriminant < 0) {
       return false;
-		}
+    }
 
     t = -b - sqrt(discriminant) / (2.0 * a);
     double3 p = r.at(t);
     double3 outward_normal = (p - center) / radius;
-		bool front = dot(r.dir, outward_normal);
-		normal = front ? outward_normal : -outward_normal;
+    bool front = dot(r.dir, outward_normal);
+    normal = front ? outward_normal : -outward_normal;
 
     return true;
   }
