11035ece6ea97b15ac729cf33b67815002bce97c
shangar21 <sharanshangar@gmail.com>
2024-08-16 12:48:29 -0400

    cleanup and format

--- cuda/utils.cu
+++ cuda/utils.cu
@@ -22,8 +22,8 @@ __host__ __device__ double3 unit_vector(const double3 &v) {
 }
 
 // Convert double* to double3
-__host__ __device__ double3 doublePtrToDouble3(const double* ptr) {
-    return make_double3(ptr[0], ptr[1], ptr[2]);
+__host__ __device__ double3 doublePtrToDouble3(const double *ptr) {
+  return make_double3(ptr[0], ptr[1], ptr[2]);
 }
 
 // Length squared of a double3 vector
@@ -32,55 +32,63 @@ __host__ __device__ double length_squared(const double3 &v) {
 }
 
 // Addition of two double3 vectors
-__host__ __device__ double3 operator+(const double3& a, const double3& b) {
-    return make_double3(a.x + b.x, a.y + b.y, a.z + b.z);
+__host__ __device__ double3 operator+(const double3 &a, const double3 &b) {
+  return make_double3(a.x + b.x, a.y + b.y, a.z + b.z);
 }
 
 // Subtraction of two double3 vectors
-__host__ __device__ double3 operator-(const double3& a, const double3& b) {
-    return make_double3(a.x - b.x, a.y - b.y, a.z - b.z);
+__host__ __device__ double3 operator-(const double3 &a, const double3 &b) {
+  return make_double3(a.x - b.x, a.y - b.y, a.z - b.z);
 }
 
 // Multiplication of a double3 vector by a scalar
-__host__ __device__ double3 operator*(const double3& v, double scalar) {
-    return make_double3(v.x * scalar, v.y * scalar, v.z * scalar);
+__host__ __device__ double3 operator*(const double3 &v, double scalar) {
+  return make_double3(v.x * scalar, v.y * scalar, v.z * scalar);
 }
 
 // Multiplication of a scalar by a double3 vector
-__host__ __device__ double3 operator*(double scalar, const double3& v) {
-    return make_double3(v.x * scalar, v.y * scalar, v.z * scalar);
+__host__ __device__ double3 operator*(double scalar, const double3 &v) {
+  return make_double3(v.x * scalar, v.y * scalar, v.z * scalar);
 }
 
 // Division of a double3 vector by a scalar
-__host__ __device__ double3 operator/(const double3& v, double scalar) {
-    return make_double3(v.x / scalar, v.y / scalar, v.z / scalar);
+__host__ __device__ double3 operator/(const double3 &v, double scalar) {
+  return make_double3(v.x / scalar, v.y / scalar, v.z / scalar);
 }
 
 // Compound assignment: addition
-__host__ __device__ double3& operator+=(double3& a, const double3& b) {
-    a.x += b.x; a.y += b.y; a.z += b.z;
-    return a;
+__host__ __device__ double3 &operator+=(double3 &a, const double3 &b) {
+  a.x += b.x;
+  a.y += b.y;
+  a.z += b.z;
+  return a;
 }
 
 // Compound assignment: subtraction
-__host__ __device__ double3& operator-=(double3& a, const double3& b) {
-    a.x -= b.x; a.y -= b.y; a.z -= b.z;
-    return a;
+__host__ __device__ double3 &operator-=(double3 &a, const double3 &b) {
+  a.x -= b.x;
+  a.y -= b.y;
+  a.z -= b.z;
+  return a;
 }
 
 // Compound assignment: multiplication by a scalar
-__host__ __device__ double3& operator*=(double3& v, double scalar) {
-    v.x *= scalar; v.y *= scalar; v.z *= scalar;
-    return v;
+__host__ __device__ double3 &operator*=(double3 &v, double scalar) {
+  v.x *= scalar;
+  v.y *= scalar;
+  v.z *= scalar;
+  return v;
 }
 
 // Compound assignment: division by a scalar
-__host__ __device__ double3& operator/=(double3& v, double scalar) {
-    v.x /= scalar; v.y /= scalar; v.z /= scalar;
-    return v;
+__host__ __device__ double3 &operator/=(double3 &v, double scalar) {
+  v.x /= scalar;
+  v.y /= scalar;
+  v.z /= scalar;
+  return v;
 }
 
 // Unary minus (negation) for double3
-__host__ __device__ double3 operator-(const double3& v) {
-    return make_double3(-v.x, -v.y, -v.z);
+__host__ __device__ double3 operator-(const double3 &v) {
+  return make_double3(-v.x, -v.y, -v.z);
 }
